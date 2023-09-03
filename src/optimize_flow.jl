cfcf5a325dc356b06258cc314c785f6d0efda51e
Michael Dudkowiak <michael.dudkowiak@tum.de>
2023-09-03 13:20:22 +0200

    Add handling of ScaleShiftModules in optimization, adjust tests

--- src/optimize_flow.jl
+++ src/optimize_flow.jl
@@ -47,7 +47,7 @@ export mvnormal_negll_flow_grad
                   nbatches::Integer = 100, 
                   nepochs::Integer = 100,
                   optstate = Optimisers.setup(optimizer, deepcopy(initial_flow)),
-                  negll_history = Vector{Float64}(),
+                  loss_history = Vector{Float64}(),
                   shuffle_samples::Bool = false
         )
 
@@ -57,7 +57,7 @@ number of batches and epochs to use during training.
 If desired, set `shuffle_samples` to `true` to have the samples be shuffled in between epochs. This may 
 improve the training, but increase training time.
 
-Returns a tuple `(optimized_flow, final_optimizer_state, negll_history)` where `negll_history` is a vector 
+Returns a tuple `(optimized_flow, final_optimizer_state, loss_history)` where `loss_history` is a vector 
 containing the values of the loss function during training.
 """
 function optimize_flow(smpls::VectorOfSimilarVectors{<:Real}, 
@@ -66,25 +66,29 @@ function optimize_flow(smpls::VectorOfSimilarVectors{<:Real},
                        nbatches::Integer = 100, 
                        nepochs::Integer = 100,
                        optstate = Optimisers.setup(optimizer, deepcopy(initial_flow)),
-                       negll_history = Vector{Float64}(),
+                       loss_history = Vector{Float64}(),
                        shuffle_samples::Bool = false
     )
+    if initial_flow isa ScaleShiftModule
+        return (result = initial_flow, optimizer_state = nothing, loss_history = nothing)
+    end
+
     batchsize = round(Int, length(smpls) / nbatches)
     batches = collect(Iterators.partition(smpls, batchsize))
     flow = deepcopy(initial_flow)
     state = deepcopy(optstate)
-    negll_hist = Vector{Float64}()
+    loss_hist = Vector{Float64}()
     for i in 1:nepochs
         for batch in batches
-            negll, d_flow = mvnormal_negll_flow_grad(flow, flatview(batch))
+            loss, d_flow = mvnormal_negll_flow_grad(flow, flatview(batch))
             state, flow = Optimisers.update(state, flow, d_flow)
-            push!(negll_hist, negll)
+            push!(loss_hist, loss)
         end
         if shuffle_samples
             batches = collect(Iterators.partition(shuffle(smpls), batchsize))
         end
     end
-    (result = flow, optimizer_state = state, negll_history = vcat(negll_history, negll_hist))
+    (result = flow, optimizer_state = state, loss_history = vcat(loss_history, loss_hist))
 end
 export optimize_flow
 
@@ -99,16 +103,16 @@ function optimize_flow_sequentially(smpls::VectorOfSimilarVectors{<:Real},
 
     optimized_modules = Vector{AbstractFlow}(undef, length(initial_flow.flow.fs))
     module_optimizer_states = Vector(undef, length(initial_flow.flow.fs))
-    module_negll_hists = Vector{Vector}(undef, length(initial_flow.flow.fs))
+    module_loss_hists = Vector{Vector}(undef, length(initial_flow.flow.fs))
 
     for (i,flow_module) in enumerate(initial_flow.flow.fs)
-        opt_module, opt_state, negll_hist = optimize_flow_sequentially(smpls, flow_module, optimizer; nbatches, nepochs, shuffle_samples)
+        opt_module, opt_state, loss_hist = optimize_flow_sequentially(smpls, flow_module, optimizer; nbatches, nepochs, shuffle_samples)
         optimized_modules[i] = opt_module
         module_optimizer_states[i] = opt_state
-        module_negll_hists[i] = negll_hist
+        module_loss_hists[i] = loss_hist
     end
 
-    (CompositeFlow(optimized_modules), module_optimizer_states,  module_negll_hists)
+    (result = CompositeFlow(optimized_modules), optimizer_states = module_optimizer_states, loss_histories = module_loss_hists)
 end
 
 function optimize_flow_sequentially(smpls::VectorOfSimilarVectors{<:Real}, 
@@ -120,17 +124,21 @@ function optimize_flow_sequentially(smpls::VectorOfSimilarVectors{<:Real},
     )
     @argcheck !(initial_flow isa AbstractFlowBlock) throw DomainError("The input flow is an individual flow block, please use `optimize_flow()`[@ref] to optimize flow blocks.")
     
-    optimized_blocks = Vector{AbstractFlow}(undef, length(initial_flow.flow_module.fs))
-    block_optimizer_states = Vector{NamedTuple}(undef, length(initial_flow.flow_module.fs))
-    block_negll_hists = Vector{Vector}(undef, length(initial_flow.flow_module.fs))
-
-    for (i,block) in enumerate(initial_flow.flow_module.fs)
-        opt_flow, opt_state, negll_hist = optimize_flow(smpls, block, optimizer; nbatches, nepochs, shuffle_samples = shuffle_samples)
-        optimized_blocks[i] = opt_flow
-        block_optimizer_states[i] = opt_state
-        block_negll_hists[i] = negll_hist
+    if initial_flow isa ScaleShiftModule
+        return (result = initial_flow, optimizer_states = nothing, loss_hists = nothing)
     end
 
-    (typeof(initial_flow)(optimized_blocks), block_optimizer_states,  block_negll_hists)
+    optimized_blocks = Vector{AbstractFlow}(undef, length(initial_flow.flow_module.fs))
+    block_optimizer_states = Vector{NamedTuple}(undef, length(initial_flow.flow_module.fs))
+    block_loss_hists = Vector{Vector}(undef, length(initial_flow.flow_module.fs))
+
+    for (i,block) in enumerate(initial_flow.flow_module.fs)
+        opt_flow, opt_state, loss_hist = optimize_flow(smpls, block, optimizer; nbatches, nepochs, shuffle_samples = shuffle_samples)
+        optimized_blocks[i] = opt_flow
+        block_optimizer_states[i] = opt_state
+        block_loss_hists[i] = loss_hist
+    end
+
+    (result = typeof(initial_flow)(optimized_blocks), optimizer_states = block_optimizer_states,  loss_hists = block_loss_hists)
 end
 export optimize_flow_sequentially
